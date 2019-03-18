16f7fc5d6dac23459346066137886db368fad666
Will Robbins <will@sighten.io>
2019-03-18 14:41:26 -0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './modules/main/app.module';
+import * as helmet from 'helmet';
+import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import * as rateLimit from 'express-rate-limit';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+  app.use(helmet());
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  }));
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
